54a6749379f3e8f7c1f3fb621e64d1767edd32a3
suchen-zhang <2670038460@qq.com>
2024-10-22 22:38:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
-    result.min = ;
-    result.mid = ;
-    result.max = ;// 在这里完成你的代码
+    result.min = 13;
+    result.mid = 15;
+    result.max = 17;// 在这里完成你的代码
 }
